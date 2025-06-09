9a5b92589b75f37562d4b6688e38c613c598e309
ghaith <>
2025-06-09 23:02:31 +0300

    remove import

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,11 @@
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
-import { HomePage } from "@/screens/HomePage/HomePage";
 import { TopProducts } from "@/components/TopProducts";
 import { ContactSection } from "@/components/ContactSection";
 
 export default function Home() {
 	return (
 		<>
-			{/* <HomePage /> */}
 			<HeroSection />
 			<AboutSection />
 			{/* <TopProducts /> */}
